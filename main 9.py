67b4f1098b518d7abc96fa9a2b63f62f1151db88
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-10-10 01:23:15 -0600

    Add files via upload

---
+++ main 9.py
@@ -0,0 +1,34 @@
+print(" ")
+print("Arzaba Diaz April 3W 1173")
+def sum(lista):
+    """
+    Esta funcion recibe una lista de numeros y devuelve la suma de todos los elementos.
+    
+    Args:
+    lista (list): Una lista de numeros.
+    
+    Returns:
+    float: La suma de todos los numeros en la lista.
+    """
+    total = 0
+    for num in lista:
+        total += num
+    return total
+
+def multip(lista):
+    """
+    Esta funcion recibe una lista de numeros y devuelve el producto de todos los elementos.
+    
+    Args:
+    lista (list): Una lista de numeros.
+    
+    Returns:
+    float: El producto de todos los numeros en la lista.
+    """
+    total = 1
+    for num in lista:
+        total *= num
+    return total
+
+
+  
